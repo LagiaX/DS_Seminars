d8eca5fe38394374108fa4ea05a55478d2854c6a
German Cabeza de Vaca Roman <ivory_lagiax@hotmail.com>
2015-03-12 14:05:12 +0100

    Update song of beer.py
    No major changes. Just one more print.

--- song of beer.py
+++ song of beer.py
@@ -1,2 +1,3 @@
-for x in range(99,0,-1):
-    print x,"bottles of beer on the wall,",x,"bottles of beer.\nTake one down, pass it around,",(x-1),"bottles of beer on the wall. "
+for wall in range(99,0,-1):
+    print wall,"bottles of beer on the wall,",wall,"bottles of beer.\nTake one down, pass it around,",(wall-1),"bottles of beer on the wall."
+print "No more bottles of beer on the wall, no more bottles of beer."
