ac7f6e00a4a06b6427a0baa0724265d6d1bf4f85
German Cabeza de Vaca Roman <loganlagiax@gmail.com>
2015-03-20 20:25:58 +0100

    Folder name changed.

--- Song of Beer/songOfBeer.py
+++
@@ -1,3 +0,0 @@
-for wall in range(99,0,-1):
-    print wall,"bottles of beer on the wall,",wall,"bottles of beer.\nTake one down, pass it around,",(wall-1),"bottles of beer on the wall."
-print "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall..."
